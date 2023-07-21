65247a9b2bb3fc5b52003680b51bca248e12c83a
jamesbarker <jamesbarker15@aol.com>
2023-07-21 19:33:45 +0100

    added script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-touch file1
-touch file2
+
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt install git -y
+
+mkdir /home/ubuntu/webscraper
+cd /home/ubuntu/webscraper
+git clone https://github.com/jamesbarker15/web-scraper-prod.git
+cd /home/ubuntu/webscraper/web-scraper-prod
+sudo NEEDRESTART_MODE=a apt install python3-pip -y
+sudo pip install -r requirements.txt
+python3 main.py
+
