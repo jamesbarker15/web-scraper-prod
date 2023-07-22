ab2c75cc184b1bf4832030a642ab9f3977f139db
jamesbarker <jamesbarker15@aol.com>
2023-07-22 11:15:48 +0100

    added further script.

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-cd /home/ubuntu/webscraper/web-scraper-prod &&
 sudo NEEDRESTART_MODE=a apt install python3-pip -y &&
-sudo pip install -r requirements.txt &&
-sudo python3 main.py
+sudo pip install -r /home/ubuntu/webscraper/web-scraper-prod/requirements.txt &&
+sudo python3 /home/ubuntu/webscraper/web-scraper-prod/main.py
 
--- script.sh
+++ script.sh
@@ -7,9 +7,7 @@ sudo apt install git -y
 mkdir /home/ubuntu/webscraper
 cd /home/ubuntu/webscraper
 git clone https://github.com/jamesbarker15/web-scraper-prod.git
-cd /home/ubuntu/webscraper/web-scraper-prod
-sh sudo chmod +x /home/ubuntu/webscraper/web-scraper-prod/run.sh
-sh sudo ./run.sh
+sudo chmod +x /home/ubuntu/webscraper/web-scraper-prod/run.sh
 #sudo NEEDRESTART_MODE=a apt install python3-pip -y
 #sudo pip install -r requirements.txt
 
